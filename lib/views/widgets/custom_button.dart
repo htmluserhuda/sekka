02ea577de0ca85607a37ff71e8aac51eccf56c4f
huda elsayed <hdyalmsar@gmail.com>
2024-11-03 23:44:33 +0200

    editting

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,31 +1,37 @@
-// lib/views/components/custom_button.dart
+// lib/views/widgets/custom_button.dart
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
-  final Color backgroundColor;
   final Color textColor;
+  final Color? backgroundColor;
 
   const CustomButton({
     required this.text,
     required this.onPressed,
-    this.backgroundColor = Colors.transparent,
     this.textColor = Colors.white,
+    this.backgroundColor, // Optional background color
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-        foregroundColor: textColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor ?? Colors.transparent, // Fallback to transparent
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          foregroundColor: textColor,
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(color: textColor),
         ),
       ),
-      onPressed: onPressed,
-      child: Text(text),
     );
   }
 }
